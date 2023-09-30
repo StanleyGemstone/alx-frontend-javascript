8d2bd0549951276fd2afd33d0ffe9150f2fecc3a
StanleyGemstone <stanleynduchi@gmail.com>
2023-09-30 08:59:09 +0100

    redo 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,27 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+export async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userResult = await signUpUser(firstName, lastName);
+    const photoResult = await uploadPhoto(fileName);
+
+    return [
+      {
+        user: {
+          firstName: userResult.firstName,
+          lastName: userResult.lastName,
+        },
+        photo: {
+          filename: photoResult.filename,
+        },
+      },
+    ];
+  } catch (error) {
+    return [
+      {
+        error: error.message, // You can customize this part to handle errors as needed.
+      },
+    ];
+  }
 }
